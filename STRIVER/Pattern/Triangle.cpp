09b3747857eae136036d0576cfe049f4b154fe70
unknown <ritikakes33@gmail.com>
2024-03-04 18:05:45 +0530

    learning

---
+++ STRIVER/Pattern/Triangle.cpp
@@ -0,0 +1,20 @@
+/*
+
+  *
+ ***
+*****
+
+void nStarTriangle(int n) {
+    int i=1;
+    while(i<=n){
+        for(int j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+        for(int k=1;k<=2*i-1;k++){
+            cout<<"*";
+        }
+        i++;
+        cout<<endl;
+    }
+}
+*/
